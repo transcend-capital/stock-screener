2e04a22c3a7938725f13413c23ceb925cf47deb9
Transcend Capital <info@transcend-group.org>
2022-02-27 10:33:10 +0100

    Added egui

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,20 @@
+use eframe::{epi, egui};
+
+#[derive(Default)]
+struct TranscendMarkets {}
+impl epi::App for TranscendMarkets {
+    fn name(&self) -> &str {
+        "Transcend Markets"
+    }
+
+    fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+        });
+    }
+}
+
 fn main() {
-    println!("Hello World from client!");
+    let app = TranscendMarkets::default();
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(Box::new(app), native_options);
 }
