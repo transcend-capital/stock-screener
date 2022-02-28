72c07797844e1c33ada712e4af4cf7291363209b
Transcend Capital <info@transcend-group.org>
2022-02-28 14:26:23 +0100

    Added postgres

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,20 +1,23 @@
-use eframe::{epi, egui};
+use eframe::{epi, egui, epaint};
 
 #[derive(Default)]
-struct TranscendMarkets {}
-impl epi::App for TranscendMarkets {
+struct MarketsTerminal {}
+
+// Main app panel
+impl epi::App for MarketsTerminal {
     fn name(&self) -> &str {
-        "Transcend Markets"
+        "Transcend Capital - Markets Terminal"
     }
 
     fn update(&mut self, ctx: &egui::Context, frame: &epi::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
         });
-    }
+    }   
 }
 
 fn main() {
-    let app = TranscendMarkets::default();
-    let native_options = eframe::NativeOptions::default();
+    let app = MarketsTerminal::default();
+    let mut native_options = eframe::NativeOptions::default();
+    native_options.initial_window_size = Some(epaint::Vec2::new(540., 320.));
     eframe::run_native(Box::new(app), native_options);
 }
